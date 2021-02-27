3984dec7ba49286f5223b239555e940a12a2eabf
Magnus Eriksson <eriksson.mag@gmail.com>
2021-02-27 22:09:32 +0100

    Refactoring

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ const HttpListenAddr = ":8080"
 func main() {
 	initStreamdeck()
 	initGpio()
-
 	httpServer()
 }
 
@@ -68,46 +67,30 @@ func imagePath(image string) string {
 	return fmt.Sprintf("%s/%s", ImageDir, image)
 }
 
+func initImageToggleButton(image string, function func()) *buttons.ImageFileButton {
+	button, err := buttons.NewImageFileButton(imagePath(image))
+	if err != nil {
+		panic(err)
+	}
+
+	button.SetActionHandler(actionhandlers.NewCustomAction(func(streamdeck.Button) { function() }))
+	return button
+}
+
 func initStreamdeck() {
 	sd, err := streamdeck.New()
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("Found device [%s]\n", sd.GetName())
 
-	dp1Button, err := buttons.NewImageFileButton(imagePath("monitor.jpg"))
-	if err != nil {
-		fmt.Printf("Failure [${err}]")
-	} else {
-		dp1Button.SetActionHandler(actionhandlers.NewCustomAction(func(streamdeck.Button) { blinkGpioPin(Dp1PinNumber)}))
-		sd.AddButton(3, dp1Button)
-	}
-
-	dp2Button, err := buttons.NewImageFileButton(imagePath("monitor.jpg"))
-	if err != nil {
-		fmt.Printf("Failure [${err}]")
-	} else {
-		dp2Button.SetActionHandler(actionhandlers.NewCustomAction(func(streamdeck.Button) { blinkGpioPin(Dp2PinNumber)}))
-		sd.AddButton(0, dp2Button)
-	}
-
-	kbButton, err := buttons.NewImageFileButton(imagePath("keyboard.jpg"))
-	if err != nil {
-		fmt.Printf("Failure [${err}]")
-	} else {
-		kbButton.SetActionHandler(actionhandlers.NewCustomAction(func(streamdeck.Button) { blinkGpioPin(UsbPinNumber)}))
-		sd.AddButton(1, kbButton)
-	}
-
-	allButton, err := buttons.NewImageFileButton(imagePath("all.jpg"))
-	if err != nil {
-		fmt.Printf("Failure [${err}]")
-	} else {
-		allButton.SetActionHandler(actionhandlers.NewCustomAction(func(streamdeck.Button) {
-			blinkGpioPin(Dp1PinNumber)
-			blinkGpioPin(Dp2PinNumber)
-			blinkGpioPin(UsbPinNumber)
-		}))
-		sd.AddButton(4, allButton)
-	}
+	sd.AddButton(0, initImageToggleButton("monitor.jpg", func() { blinkGpioPin(Dp1PinNumber)} ))
+	sd.AddButton(1, initImageToggleButton("keyboard.jpg", func() { blinkGpioPin(UsbPinNumber)} ))
+	sd.AddButton(3, initImageToggleButton("monitor.jpg", func() { blinkGpioPin(Dp2PinNumber)} ))
+	sd.AddButton(4, initImageToggleButton("all.jpg", func() {
+		blinkGpioPin(Dp1PinNumber)
+		blinkGpioPin(Dp2PinNumber)
+		blinkGpioPin(UsbPinNumber)
+	} ))
 }
